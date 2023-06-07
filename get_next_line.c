6089b09b8cf207352c1ccca24cd87235cdc4f382
Xiru Wang <xiwang@c4b3c1.42berlin.de>
2023-06-07 12:38:19 +0200

    day2

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,16 @@
 /*   By: xiwang <xiwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:40:34 by xiwang            #+#    #+#             */
-/*   Updated: 2023/06/05 20:08:44 by xiwang           ###   ########.fr       */
+/*   Updated: 2023/06/07 12:16:52 by xiwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #define BUFFER_SIZE 100
 
 char	*get_next_line(int fd)
 {
 
 }
+
+Hello, my name is george\n your name is xiru\nMy nam i aran
