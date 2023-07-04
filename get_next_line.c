5b44688bf4cfb158c5252a55d84a524188c0a620
Xiru Wang <xiru.wang2021@gmail.com>
2023-07-04 19:53:48 +0200

    draft

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: xiruwang <xiruwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:40:34 by xiwang            #+#    #+#             */
-/*   Updated: 2023/07/03 16:29:43 by xiruwang         ###   ########.fr       */
+/*   Updated: 2023/07/04 18:43:12 by xiruwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ read() attempts to read nbyte bytes of data from the object
 fd 是一个已打开的文件描述符，从中读取数据。这个文件描述符可以是一个文件，也可以是一个设备，或者是一个 socket 等。
 buf 是一个指针，指向一个缓冲区，用于存储从文件描述符读取的数据。
 count 是请求读取的字节数
+https://github.com/jdecorte-be/42-Get-next-line/blob/master/get_next_line.c
+
+schroeder_jan@posteo.de
 */
 
 char	*get_next_line(int fd)
@@ -48,24 +51,23 @@ static char *read_file(int fd, char *stash)
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if(!buffer)
 		return (NULL);
-	read_byte = 1;
-	while (read_byte > 0)
+	// 当 read() 函数返回值为 0 时，表示已经读取到了文件的结尾（EOF）。
+	while ((read_byte = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		read_byte = read(fd, buffer, BUFFER_SIZE);
-		if (read_byte < 0)//error to read
-		{
-			free(buffer);
-			return (NULL);
-		}
-		else if (read_byte == 0)//read nothing, break the loop
-			break;
-		buffer[read_byte] = '\0';//terminate the string
+		buffer[read_byte] = '\0';//手动添加string
 		temp = stash;
 		stash = ft_strjoin(temp, buffer);
 		free(temp);
 		if (strchr(buffer, '\n'))
 			break;
 	}
+	if (read_byte == -1)//error to read
+	{
+		free(buffer);
+		return (NULL);
+	}
+	//if (read_byte == 0)//read nothing, break the loop
+	//	break;
 	free(buffer);
 	return (stash);
 }
@@ -78,7 +80,7 @@ static char	*get_line(char *stash)
 	i = 0;
 	while(stash[i] && stash[i]!= '\n')
 		i++;
-	line = (char *)malloc(i + 2);//为目标行的字符数（包括换行符）加上空字符预留的空间，总共 i + 2 个字符的空间。
+	line = (char *)malloc(i + 2);//为目标行的字符数（包括换行符）加上空字符预留的空间
 	if(!line)
 		return (NULL);
 	i = 0;
@@ -117,3 +119,13 @@ static char *reset_stash(char *stash)
 	free(stash);//复制完剩饭，free原来的
 	return (new);
 }
+
+
+int	main()
+{
+	int fd = open("filename.txt", O_RDONLY);
+	get_next_line(fd);
+	close(fd);
+	return (0);
+}
+//...ErQSHTv6ewY3NF8//
